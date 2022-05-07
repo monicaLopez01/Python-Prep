c690f92bd9378e508def05da412f427bcb9ee4cc
MonicadelaCruz01 <quihubolin75@gmail.com>
2022-05-06 21:21:46 -0500

    Tarea02

---
+++ 02 - Variables y Tipos de Datos/Mitarea02.py
@@ -0,0 +1,64 @@
+x=8
+print(x)
+
+type(8.5)
+
+type (x)
+
+n= 'Monica Lopez de la Cruz'
+
+com= 3 + 3j
+
+type(com)
+
+pi=3.1416
+
+a='True'
+b=True
+
+print('el valor de a es:',type(a),'el valor de b es:',type(b))
+
+y= 9 + 5.22
+
+com2= 1 + 1j
+print (com + com2)
+
+su= 8 + com2
+print(su)
+
+print(8.2*2)
+
+print(5**8)
+
+c=27/4
+print(c)
+
+print(27//4)
+
+print(27%4)
+
+4*6+3
+
+nom='Nombre:'
+print(nom + n)
+
+2=='2'
+
+2==int('2')
+
+a1=float('3,8')
+
+h=3
+h-=5
+print(h)
+
+1<<2
+
+2+'2'
+
+float(2)+float('2')
+
+g= 'Gracias'
+z=3
+print(g * z )
+
